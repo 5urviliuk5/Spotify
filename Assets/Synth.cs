3907d4389157aba0654aeb881bbba51620705bf1
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-17 18:03:59 +0200

    more complicated code

--- Assets/Synth.cs
+++ Assets/Synth.cs
@@ -14,9 +14,9 @@ public class Synth : MonoBehaviour
         var samples = new float[44100 * 3];
         for (int i = 0; i < samples.Length; i++)
         {
-            samples[i] = Mathf.Sin(i / 44000f * 6.28f * frequency);
-            samples[i] += Mathf.Sin(i / 44100f * Mathf.PI * 2f * 960);
-            samples[i] *= 0.5f;
+            samples[i] = Mathf.Sin(i / 44000f * Mathf.PI * 2 * 853);
+            samples[i] += Mathf.Sin(i / 44000f * Mathf.PI * 2 * 960);
+            samples[i] /= 2;
         }
 
         clip.SetData(samples, 0);
